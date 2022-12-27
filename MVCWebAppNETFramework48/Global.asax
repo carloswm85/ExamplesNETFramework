e4a9e304c087eb1da02f48f2e56f4cfa284b31a7
carloswm85 <carloswashingtonmercado@gmail.com>
2022-12-27 09:18:44 -0300

    Add project files.

---
+++ MVCWebAppNETFramework48/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWebAppNETFramework48.MvcApplication" Language="C#" %>
---
+++ MVCWebAppNETFramework48/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCWebAppNETFramework48.Startup))]
+namespace MVCWebAppNETFramework48
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
